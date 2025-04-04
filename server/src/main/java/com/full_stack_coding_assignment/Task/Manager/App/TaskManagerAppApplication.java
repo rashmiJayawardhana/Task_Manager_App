3d04bb3d7ff8b6530e9e63467b607fc4c32e6fed
Rashmi Jayawardhana <155569390+rashmiJayawardhana@users.noreply.github.com>
2025-04-04 23:08:12 +0530

    Initialize backend

---
+++ server/src/main/java/com/full_stack_coding_assignment/Task/Manager/App/TaskManagerAppApplication.java
@@ -0,0 +1,13 @@
+package com.full_stack_coding_assignment.Task.Manager.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerAppApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/full_stack_coding_assignment/Task/Manager/App/TaskManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.full_stack_coding_assignment.Task.Manager.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
